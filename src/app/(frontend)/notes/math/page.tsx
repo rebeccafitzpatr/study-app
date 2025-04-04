dd724334f30aa7adf7eaba95d7f4335ab7b7902a
rebeccafitzpatr <rebecca.fitzpatr@gmail.com>
2025-04-04 23:43:21 +1300

    feat: added subject pages

---
+++ src/app/(frontend)/notes/math/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'MATHS notes',
+}
+
+export default function MathsPage() {
+  return (
+    <div>
+      <h1>MATHS Notes</h1>
+    </div>
+  )
+}
---
+++ src/app/(frontend)/notes/softeng/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'SOFTENG notes',
+}
+
+export default function SoftEngPage() {
+  return (
+    <div>
+      <h1>SOFTENG Notes</h1>
+    </div>
+  )
+}
